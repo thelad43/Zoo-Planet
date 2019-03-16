c5b460b3a09abbfbc9d30e81710ef3e36c671f18
Teodor <thelad43@gmail.com>
2019-03-16 10:11:11 +0200

    Code refactored

--- ZooPlanet/ZooPlanet.Web/Controllers/HomeController.cs
+++ ZooPlanet/ZooPlanet.Web/Controllers/HomeController.cs
@@ -1,24 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using ZooPlanet.Web.Models;
-
-namespace ZooPlanet.Web.Controllers
+﻿namespace ZooPlanet.Web.Controllers
 {
+    using Microsoft.AspNetCore.Mvc;
+    using Models;
+    using System.Diagnostics;
+
     public class HomeController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        public IActionResult Index() => View();
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        public IActionResult Privacy() => View();
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
