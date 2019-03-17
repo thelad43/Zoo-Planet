285d8891074f505732f2fcbf496315dd991b8fb7
Teodor <thelad43@gmail.com>
2019-03-17 19:41:25 +0200

    Created AnimalsController with action Mammals, leading to Mammals page; updated Index page and _Layout

---
+++ ZooPlanet/ZooPlanet.Web/Controllers/AnimalsController.cs
@@ -0,0 +1,9 @@
+﻿namespace ZooPlanet.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class AnimalsController : Controller
+    {
+        public IActionResult Mammals() => View();
+    }
+}
